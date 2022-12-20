b67ce16fad9c8674c1a6cf74381349f8a4e81340
Puk Sørensenn <pukx0525@stud.kea.dk>
2022-12-20 23:04:57 +0100

    build vite dims

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,11 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        fremkaldelse: resolve(__dirname, "src/fremkaldelse.html"),
+        analogguide: resolve(__dirname, "src/analogguide.html"),
+        singleview: resolve(__dirname, "src/singleview.html"),
+        smalfilmoverspillet: resolve(__dirname, "src/smalfilmoverspillet.html"),
+        sitemap: resolve(__dirname, "src/sitemap.xml"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
